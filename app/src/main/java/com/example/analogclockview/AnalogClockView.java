0110ed46a5c77ca21d87cf16ce19a57f75b1df87
Uros Radulovic <uros.radulovic23@gmail.com>
2020-08-27 22:57:26 +0200

    updated

--- app/src/main/java/com/example/analogclockview/AnalogClockView.java
+++ app/src/main/java/com/example/analogclockview/AnalogClockView.java
@@ -1,6 +1,7 @@
 package com.example.analogclockview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -13,10 +14,12 @@ import androidx.annotation.Nullable;
 import java.util.Calendar;
 
 public class AnalogClockView extends View {
+    private int hours=0;
+    private int minutes=0;
+    private int seconds=0;
 
-    private static final int BASE_WIDTH = 400 ;
-    private static final int BASE_HEIGHT = 400;
-    private Canvas canvas;
+    private static final int BASE_WIDTH = 600;
+    private static final int BASE_HEIGHT = 600;
     private Paint paint;
     private int width=0;
     private int height=0;
@@ -24,22 +27,35 @@ public class AnalogClockView extends View {
     private int handTruncation, hourHandTruncation = 0;
     private int radius = 0;
     private boolean isInit;
-    private Rect rect = new Rect();
 
     public AnalogClockView(Context context) {
         super(context);
-        init(context,null);
     }
-
+    public AnalogClockView(Context context,int hours,int minutes,int seconds) {
+        super(context);
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
     public AnalogClockView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init(context, attrs);
+        if(attrs != null){
+            TypedArray typedArray = context.
+                    obtainStyledAttributes(attrs, R.styleable.AnalogClockView);
+            hours = typedArray.getInt(R.styleable.AnalogClockView_hours, 1);
+            minutes = typedArray.getInt(R.styleable.AnalogClockView_minutes,1);
+            seconds = typedArray.getInt(R.styleable.AnalogClockView_sec,1);
+
+
+            typedArray.recycle();
+        }
     }
 
-    private void init(Context context, AttributeSet attrs){
+
+    private void initClockDrawValues(){
         height = getHeight();
-        width = getWidth();
-        padding = 50;
+        width =  getWidth();
+        padding = 0;
         int min = Math.min(height, width);
         radius = min / 2 - padding;
         handTruncation = min / 20;
@@ -47,6 +63,35 @@ public class AnalogClockView extends View {
         paint = new Paint();
         isInit = true;
     }
+    private void drawHand(Canvas canvas, double loc, boolean isHour) {
+        double angle = Math.PI * loc / 30 - Math.PI / 2;
+        int handRadius = isHour ? radius - handTruncation - hourHandTruncation : radius - handTruncation;
+        canvas.drawLine(width / 2, height / 2,
+                (float) (width / 2 + Math.cos(angle) * handRadius),
+                (float) (height / 2 + Math.sin(angle) * handRadius),
+                paint);
+    }
+    private void drawHands(Canvas canvas) {
+        drawHand(canvas, hours*5, true);
+        drawHand(canvas, minutes, false);
+        drawHand(canvas, seconds, false);
+    }
+    private void drawCircle(Canvas canvas) {
+        paint.reset();
+        paint.setColor(getResources().getColor(android.R.color.white));
+        paint.setStrokeWidth(5);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+        canvas.drawCircle(width / 2, height / 2, radius + padding - 10, paint);
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = w;
+        height = h;
+    }
+
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -76,63 +121,13 @@ public class AnalogClockView extends View {
         int smaller = Math.min(w, h);
         w = smaller;
         h = smaller;
+
         if (!areAllEqual(getPaddingTop(), getPaddingBottom(), getPaddingLeft(), getPaddingRight())) {
             w = smaller - (getPaddingTop() + getPaddingBottom());
             h = smaller - (getPaddingLeft() + getPaddingRight());
+        }
         setMeasuredDimension(w, h);
-        }
-    }
-    Rect backgroundBounds = new Rect();
-    int textSize;
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 
-        super.onSizeChanged(w, h, oldw, oldh);
-
-        width = w;
-        height = h;
-        backgroundBounds.left = getPaddingLeft();
-        backgroundBounds.top = getPaddingTop();
-        backgroundBounds.right = w - getPaddingRight();
-        backgroundBounds.bottom = h - getPaddingBottom();
-    }
-    @Override
-    protected void onDraw(Canvas canvas) {
-        if (!isInit) {
-            init();
-        }
-
-        canvas.drawColor(Color.BLACK);
-        drawCircle(canvas);
-        drawHands(canvas);
-
-        postInvalidateDelayed(500);
-        invalidate();
-    }
-    private void drawHand(Canvas canvas, double loc, boolean isHour) {
-        double angle = Math.PI * loc / 30 - Math.PI / 2;
-        int handRadius = isHour ? radius - handTruncation - hourHandTruncation : radius - handTruncation;
-        canvas.drawLine(width / 2, height / 2,
-                (float) (width / 2 + Math.cos(angle) * handRadius),
-                (float) (height / 2 + Math.sin(angle) * handRadius),
-                paint);
-    }
-
-    private void drawHands(Canvas canvas) {
-        Calendar c = Calendar.getInstance();
-        float hour = c.get(Calendar.HOUR_OF_DAY);
-        hour = hour > 12 ? hour - 12 : hour;
-        drawHand(canvas, (hour + c.get(Calendar.MINUTE) / 60) * 5f, true);
-        drawHand(canvas, c.get(Calendar.MINUTE), false);
-        drawHand(canvas, c.get(Calendar.SECOND), false);
-    }
-    private void drawCircle(Canvas canvas) {
-        paint.reset();
-        paint.setColor(getResources().getColor(android.R.color.white));
-        paint.setStrokeWidth(5);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setAntiAlias(true);
-        canvas.drawCircle(width / 2, height / 2, radius + padding - 10, paint);
     }
     private boolean areAllEqual(int... values) {
         if (values.length == 0) {
@@ -146,4 +141,35 @@ public class AnalogClockView extends View {
         }
         return true;
     }
+
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (!isInit) {
+            initClockDrawValues();
+        }
+
+        canvas.drawColor(Color.BLACK);
+        drawCircle(canvas);
+        drawHands(canvas);
+
+        seconds++;
+        if (seconds>59)
+        {
+            minutes++;
+            seconds=0;
+        }
+        if (minutes>59){
+            minutes = 0;
+            hours++;
+        }
+   /*     if (hours>12){
+            hours = 1;
+        }*/
+        postInvalidateDelayed(1000);
+
+    }
+
+
 }
--- app/src/main/java/com/example/analogclockview/MainActivity.java
+++ app/src/main/java/com/example/analogclockview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.analogclockview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+/*        LinearLayout linearLayout = findViewById(R.id.mainLayout);
+        linearLayout.addView(new AnalogClockView(this));*/
+// kod za programabilno dodavanje
     }
 }
